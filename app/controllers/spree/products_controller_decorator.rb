46bfd93312169dcb6002be9ce54b9a617ea1030b
pulkit21 <pulkit.ag02@gmail.com>
2015-04-29 17:44:05 +0530

    removed extra code

--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -1,4 +1,4 @@
-Spree::ProductsController.class_eval do
+Spree::Api::ProductsController.class_eval do
   helper Spree::ReviewsHelper
 
   [:avg_rating, :reviews_count].each { |attrib| Spree::PermittedAttributes.product_attributes << attrib }
